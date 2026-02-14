b10bd0975d83a319c719fe6d0ca0870d27ebf1f0
Aristide <aristide.bamazi@imsp-uac.org>
2026-02-14 09:10:16 +0000

    Improving tui

--- tui.go
+++ tui.go
@@ -1,12 +1,27 @@
 package typer
 
 import (
+	"fmt"
 	"strings"
 	"unicode"
 
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	titleStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Right = "├"
+		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 1)
+	}()
+
+	infoStyle = func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		b.Left = "┤"
+		return titleStyle.BorderStyle(b)
+	}()
 )
 
 type Model struct {
@@ -30,7 +45,14 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.WindowSizeMsg:
 		m.width = msg.Width
 		m.height = msg.Height
-		m.viewport = viewport.New(msg.Width, msg.Height)
+
+		inputHeight := lipgloss.Height(m.inputView())
+		headerHeight := lipgloss.Height(m.headerView())
+		footerHeight := lipgloss.Height(m.footerView())
+		verticalMarginHeight := headerHeight + footerHeight + inputHeight
+
+		m.viewport = viewport.New(m.width, m.height-verticalMarginHeight)
+		m.viewport.YPosition = headerHeight
 		// wrap text with lipgloss.NewStyle().Width(m.width).Render(...)
 		// https://github.com/charmbracelet/bubbles/issues/56#issuecomment-1073306054
 		m.viewport.SetContent(lipgloss.NewStyle().Width(m.width).Render(m.renderText()))
@@ -47,6 +69,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			char := msg.Runes[0]
 			if unicode.IsPrint(char) {
 				m.app.HandleKey(char)
+				m.viewport.SetContent(lipgloss.NewStyle().Width(m.width).Render(m.renderText()))
 			}
 		}
 	default:
@@ -57,14 +80,40 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m Model) View() string {
-	return m.viewport.View()
+	return fmt.Sprintf("%s\n%s\n%s\n%s", m.inputView(), m.headerView(), m.viewport.View(), m.footerView())
 }
 
 func (m Model) renderText() string {
 	var buf strings.Builder
-	for _, word := range m.app.Words() {
-		buf.WriteString(string(word.Text))
+	currentWordStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("#9BCED7")).Bold(true)
+	for idx, word := range m.app.Words() {
+		text := string(word.Text)
+		if idx == m.app.CurrentWordIndex() {
+			text = currentWordStyle.Render(text)
+		}
+		buf.WriteString(text)
 		buf.WriteString(" ")
 	}
 	return buf.String()
+}
+
+func (m Model) headerView() string {
+	title := titleStyle.Render("Chapter")
+	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(title)))
+	return lipgloss.JoinHorizontal(lipgloss.Center, title, line)
+}
+
+func (m Model) footerView() string {
+	info := infoStyle.Render(fmt.Sprintf("%3.f%%", m.viewport.ScrollPercent()*100))
+	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(info)))
+	return lipgloss.JoinHorizontal(lipgloss.Center, line, info)
+}
+
+func (m Model) inputView() string {
+	index := m.app.CurrentWordIndex()
+	if index == -1 {
+		return ">"
+	}
+	word := m.app.Words()[index]
+	return "> " + string(word.Progress)
 }
